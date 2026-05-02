57e49b4dceeb277dbdbeaa8522cceb61b44ea64e
Davi0268 <dvlcruz.07@gmail.com>
2026-05-02 19:56:44 -0300

    Zero para cancelar

---
+++ zeroPcan.cpp
@@ -0,0 +1,24 @@
+// Zero para cancelar prova 21
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int T = 0;
+    int t;
+    cin >> t;
+    int nums[t];
+    for(int i = 0; i < t; i++){
+        cin >> nums[i];
+    }
+    for(int j = t; j > 0; j--){
+        if(nums[j] == 0){
+            nums[j - 1] = 0;
+        }
+    }
+    for(int x =0; x < t; x++){
+        T = nums[x] + T;
+    }
+    cout << T;
+    return 0;
+}
