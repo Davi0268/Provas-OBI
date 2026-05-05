9e297db38413bf9e216c0ed148d87ae3a669a6b2
Davi0268 <dvlcruz.07@gmail.com>
2026-05-05 20:30:13 -0300

    Testando
    mudei a ordem de adição no vetor de "-" pra "+", adicionei  o calculo que deve ser feito no final, vim testando if's e algumas funções da biblioteca vector

--- zeroPcan.cpp
+++ zeroPcan.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void addvector(vector<int>& v, int x){
     
-    for(int j = 0; j < x; j++){
+    for(int j = x; j > 0; j--){
         int z;
         cin >> z;
         v.push_back(z);
@@ -12,20 +12,30 @@ void addvector(vector<int>& v, int x){
 }
 
 int main(){
+    int as = 0;
     int tamanho;
     cin >> tamanho;
     vector<int> valores ={};
     addvector(valores, tamanho);
-    for(int j = tamanho; j >= 0; j--){
-        if(valores[j] == 0){
-            
-            
-            valores.erase(valores.begin() + j);
-           // valores.erase(valores.begin() + (j - 1));
+    for(int j = 0; j < tamanho; j++){
+        if(valores[j] == 0 ){
+            if(valores[j - 1] = 0){
+                valores[j - 2] = 0;
+            }
+            valores[j - 1] = 0;
+            //valores[j - 1] = 0;
+        //valores.erase(valores.begin() + j);
+        // valores.erase(valores.begin() + (j - 1));
         }
-    }
+    }/*
+    if(z == 0){
+            v[j - 1] = 0;
+        }    
+    */
     for(int i = 0; i < tamanho; i++){
         cout << valores[i] << " ";
+        //as = valores[i] + as;
     }
+    //cout << as;
     return 0;
 }
