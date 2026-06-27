2ea8a0bb20a622766cbadbdaf3238261da637833
Davi0268 <dvlcruz.07@gmail.com>
2026-06-27 19:58:25 -0300

    tentei
    fiz poucas alterações

--- aceldePart.cpp
+++ aceldePart.cpp
@@ -1,21 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void acelerador(int km){
+int acelerador(int km){
     int cont = 0; 
-    km = km - 2;
-    for(int i = 0; i < km; i++){
+    int j;
+    //cout << km;
+    // 1 indexado
+    for(int i = 1; i <= km; i++){
         cont++;
         if(cont == 2){
-            int j = i + 2;
+            j = i + 2;
             if(j == km){
-                cout << "1";
+                return 1;
+               
+            }
+        }
+         if(cont == 3){
+            j = i + 2;
+            if(j == km){
+                return 2;
+                break;
+            }
+        } if(cont == 4){
+            j = i + 2;
+            if(j == km){
+                return 3;
                 break;
             }
         }
+        //cout << cont;
         if(cont == 8){
             cont = 0;
-        }
+        }    
     }
 }
 
@@ -24,7 +40,9 @@ int main(){
     int km;
     cin >> km;
 
-    acelerador(km);
-
+    if(acelerador(km) == 1 || acelerador(km) == 2 || acelerador(km) == 3 ){
+        cout << "ave meu D";
+    }
+    
     return 0;
 }
